5b2e7a279ce3459d69ed016cb557b5dc158c3655
zodiacdignity <joakim_espe@hotmail.com>
2025-10-27 20:20:14 +0100

    small update

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 let tiles = document.querySelectorAll(".cell")
 let playerTurn = 1
 
+tiles.forEach((el, i) => el.dataset.index = i)
+
+let board = Array(9).fill(null);
+
 tiles.forEach((element) =>{
     element.addEventListener("click", () => {   
         if (playerTurn == 1){
             playerTurn = 2
-            element.style.backgroundColor = 'blue';
+            element.innerHTML = 'X';
         } else {
             playerTurn = 1
-            element.style.backgroundColor = 'red';
+            element.innerHTML = 'O';
         }
+        winner(board)
     })
 })
