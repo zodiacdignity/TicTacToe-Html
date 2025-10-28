0c1147e1743effdb595e06745060c4de8e147093
zodiacdignity <joakim_espe@hotmail.com>
2025-10-28 07:53:47 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,15 +5,19 @@ tiles.forEach((el, i) => el.dataset.index = i)
 
 let board = Array(9).fill(null);
 
-tiles.forEach((element) =>{
+tiles.forEach((element, i) =>{
     element.addEventListener("click", () => {   
         if (playerTurn == 1){
             playerTurn = 2
             element.innerHTML = 'X';
+            i = "X"
+            board[i] = "X"
+           console.log(board[i])
         } else {
             playerTurn = 1
             element.innerHTML = 'O';
+            board[i] = "O"
+            console.log(board[i])
         }
-        winner(board)
     })
 })
